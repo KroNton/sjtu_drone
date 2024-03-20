bf06dd59a0416b447af6d89db1498202646f0cb6
KroNton <kyrlosfekry@gmail.com>
2024-03-20 19:52:07 +0200

    add take off and lands control

--- src/teleop_twist_keyboard.py
+++ src/teleop_twist_keyboard.py
@@ -21,6 +21,10 @@ For Holonomic mode (strafing), hold down the shift key:
    J    K    L
    M    <    >
 
+   
+1 :  make Quadrotor takes off
+2 :  make Quadrotor lands
+
 t : up (+z)
 b : down (-z)
 
